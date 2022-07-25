727e1154b1895869106e092b5c30372ea76dd5ee
Anupam Rao <anupamrao7777@gmail.com>
2022-07-25 20:49:36 +0530

    First commit

--- MVVMDesignPattern/ViewController.swift
+++ MVVMDesignPattern/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("first test log")
         // Do any additional setup after loading the view.
     }
 
